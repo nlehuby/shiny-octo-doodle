9ae120edd8fbd7a0af47d90a8bec9910210213cf
Noémie <noemie.lehuby@zaclys.net>
2019-06-21 08:14:19 +0200

    update tools

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 set -ev
 
 # install osm-transit-extractor
-wget https://github.com/CanalTP/osm-transit-extractor/releases/download/v0.1.4/osm_transit_extractor_0_1_4 --output-document=osm_transit_extractor
+#wget https://github.com/CanalTP/osm-transit-extractor/releases/download/v0.1.4/osm_transit_extractor_0_1_4 --output-document=osm_transit_extractor
+wget https://github.com/CanalTP/osm-transit-extractor/releases/download/v0.2.1/osm_transit_extractor_v0.2.1-x86_64-linux.zip
+unzip osm_transit_extractor_v0.2.1-x86_64-linux.zip
 chmod +x osm_transit_extractor
 
 # install xsv
